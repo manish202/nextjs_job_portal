f63ea5297b793f9fc01bf30c6c63ac707cdf59ba
manish202 <godofmind51616@gmail.com>
2026-08-17 10:48:59 +0530

    file renamed from page.js to page.tsx

--- src/app/dashboard/employer/post-job/page.tsx
+++ src/app/dashboard/employer/post-job/page.tsx
@@ -1,6 +1,6 @@
 import {EmployerJobPostingAddForm} from "@/components/dashboard/EmployerJobPostingForm";
 
-const EmployerJobPostingPage = async () => {
+const EmployerJobPostingPage = () => {
     return (
         <div className="space-y-8">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">Post A New Job</h1>
